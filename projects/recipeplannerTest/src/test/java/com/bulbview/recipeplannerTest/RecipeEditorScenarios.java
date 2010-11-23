e78f69aebde97d4cf6aa9079fb8d0a1b3e4a5353
tund.aw <tund.aw@8f7103dd-5371-4b59-6c16-cf9fde0a6d44>
2010-11-23 08:24:30 +0000

    Complete - task : Should view recipe's existing ingredients in editor

--- projects/recipeplannerTest/src/test/java/com/bulbview/recipeplannerTest/RecipeEditorScenarios.java
+++ projects/recipeplannerTest/src/test/java/com/bulbview/recipeplannerTest/RecipeEditorScenarios.java
@@ -1,23 +1,29 @@
 package com.bulbview.recipeplannerTest;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static tumbler.Tumbler.Given;
+import static tumbler.Tumbler.Then;
+import static tumbler.Tumbler.When;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import tumbler.Scenario;
-import tumbler.Tumbler;
 import tumbler.TumblerRunner;
 
 import com.bulbview.recipeplanner.datamodel.Ingredient;
 import com.bulbview.recipeplanner.datamodel.Recipe;
 import com.bulbview.recipeplanner.persistence.RecipeplannerPersistenceService;
 import com.bulbview.recipeplanner.ui.RecipeEditorFormView;
-import com.bulbview.recipeplanner.ui.presenter.Category;
 import com.bulbview.recipeplanner.ui.presenter.RecipeEditorPresenter;
 
 @RunWith(TumblerRunner.class)
@@ -25,11 +31,20 @@ public class RecipeEditorScenarios {
 
     @Mock
     private RecipeplannerPersistenceService mockPersistenceService;
-
     @Mock
     private Recipe                          mockRecipe;
     @Mock
     private RecipeEditorFormView            mockRecipeEditorFormView;
+
+    private RecipeEditorPresenter           recipeEditorPresenter;
+    @Mock
+    private Ingredient                      mockIngredient1;
+    @Mock
+    private Ingredient                      mockIngredient;
+
+    public void createRecipeEditorPresenter() {
+        recipeEditorPresenter = new RecipeEditorPresenter(mockPersistenceService, mockRecipeEditorFormView, null, null);
+    }
 
     @Before
     public void setup() {
@@ -37,20 +52,34 @@ public class RecipeEditorScenarios {
     }
 
     @Scenario
-    public void shouldProvideIngredientOptionsIfEditingAnExistingRecipe() {
-        Given("The recipe editor");
-        final RecipeEditorPresenter recipeEditorPresenter = new RecipeEditorPresenter(mockPersistenceService,
-                                                                                      mockRecipeEditorFormView, null,
-                                                                                      null);
-        Tumbler.When("An existing recipe is edited");
+    public void shouldAddExistingRecipesIngredientsToEditor() {
+
+        Given("The recipe editor and, an existing recipe with ingredients");
+        createRecipeEditorPresenter();
+        final List<Ingredient> ingredients = Arrays.asList(mock(Ingredient.class));
+        given(mockRecipe.getIngredients()).willReturn(ingredients);
+
+        When("the recipe is edited in the editor");
         recipeEditorPresenter.onEditRecipe(mockRecipe);
 
-        Tumbler.Then("All ingredients are retrieved from persistence");
-        verify(mockPersistenceService).getIngredients();
-        Tumbler.Then("The ingredients are added to the editor");
-        verify(mockRecipeEditorFormView).activate(Mockito.eq(mockRecipe),
-                                                  Mockito.anyCollectionOf(Ingredient.class),
-                                                  Mockito.anyCollectionOf(Category.class));
+        Then("the recipe's existing ingredients are displayed in the editor");
+        verify(mockRecipeEditorFormView).setRecipe(mockRecipe);
+    }
 
+    @Scenario
+    public void shouldProvideIngredientOptionsIfEditingAnExistingRecipe() {
+        Given("The recipe editor");
+        createRecipeEditorPresenter();
+        final List<Ingredient> mockIngredientCollection = Arrays.asList(mockIngredient, mockIngredient1);
+        given(mockPersistenceService.getIngredients()).willReturn(mockIngredientCollection);
+
+        When("An existing recipe is edited");
+        recipeEditorPresenter.onEditRecipe(mockRecipe);
+
+        Then("All ingredients are retrieved from persistence and the ingredients are added to the editor");
+        verify(mockPersistenceService).getIngredients();
+        final InOrder inOrder = inOrder(mockRecipeEditorFormView);
+        inOrder.verify(mockRecipeEditorFormView).setIngredientOptions(mockIngredientCollection);
+        inOrder.verify(mockRecipeEditorFormView).setRecipe(mockRecipe);
     }
 }
