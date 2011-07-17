225960daffab68fa94c6d11cf3291c36b344b2f2
bulbview <info@bulbview.com>
2011-07-17 10:54:22 +0100

    CLOSED - task 34: Should not be able to activate schedule if schedule start date has passed https://github.com/bulbview/recipeplanner/issues/issue/34

--- projects/recipeplanner/src/main/java/com/bulbview/recipeplanner/ui/presenter/ShoppingListPresenter.java
+++ projects/recipeplanner/src/main/java/com/bulbview/recipeplanner/ui/presenter/ShoppingListPresenter.java
@@ -1,5 +1,6 @@
 package com.bulbview.recipeplanner.ui.presenter;
 
+import java.util.Collection;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,34 +17,33 @@ import com.bulbview.recipeplanner.ui.manager.ShoppingListCategory;
 import com.bulbview.recipeplanner.ui.manager.ShoppingListCategoryFactory;
 import com.google.appengine.repackaged.com.google.common.base.Preconditions;
 import com.google.appengine.repackaged.com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.vaadin.ui.Window;
 
 @Component
 public class ShoppingListPresenter extends Presenter<ShoppingList> implements SessionPresenter {
     
     @Autowired
-    private EntityDao<ItemCategory>           categoryDao;
+    private EntityDao<ItemCategory>                categoryDao;
     @Autowired
-    private EntityDao<Item>                   itemDao;
+    private EntityDao<Item>                        itemDao;
     @Autowired
-    private Window                            rootWindow;
-    private Map<String, ShoppingListCategory> shoppingListCategories;
-    private ShoppingListCategoryFactory       shoppingListCategoryFactory;
+    private Window                                 rootWindow;
+    private Map<String, ShoppingListCategory>      shoppingListCategories;
+    private ShoppingListCategoryFactory            shoppingListCategoryFactory;
     @Autowired
-    private Window                            shoppingListWindow;
+    private Window                                 shoppingListWindow;
+    private final Collection<ShoppingListCategory> visibleCategories;
     
     public ShoppingListPresenter() {
         this.shoppingListCategories = Maps.newHashMap();
+        this.visibleCategories = Sets.newHashSet();
     }
     
     public void addItem(final ScheduledItem scheduledItem) {
         if (scheduledItem instanceof Item) {
             final Item item = (Item) scheduledItem;
-            final ItemCategory category = getCategoryFromPersistence(item);
-            Preconditions.checkNotNull(category, "category not defined for: " + item);
-            final ShoppingListCategory shoppingListCategory = resolveOrCreateShoppingListCategory(category.getName());
-            addToView(shoppingListCategory);
-            addItemToShoppingListCategory(shoppingListCategory, item);
+            addToShoppingListCategory(item);
         }
         else if (scheduledItem instanceof Recipe) {
             final Recipe recipe = (Recipe) scheduledItem;
@@ -79,9 +79,15 @@ public class ShoppingListPresenter extends Presenter<ShoppingList> implements Se
         super.setView(shoppingList);
     }
     
-    private void addItemToShoppingListCategory(final ShoppingListCategory shoppingListCategory, final Item item) {
+    private void addToShoppingListCategory(final Item item) {
+        final ItemCategory category = getCategoryFromPersistence(item);
+        Preconditions.checkNotNull(category, "category not defined for: " + item);
+        final ShoppingListCategory shoppingListCategory = resolveOrCreateShoppingListCategory(category.getName());
+        if (!visibleCategories.contains(shoppingListCategory)) {
+            visibleCategories.add(shoppingListCategory);
+            addToView(shoppingListCategory);
+        }
         shoppingListCategory.addListItem(item);
-        
     }
     
     private void addToView(final ShoppingListCategory shoppingListCategory) {
@@ -89,7 +95,10 @@ public class ShoppingListPresenter extends Presenter<ShoppingList> implements Se
     }
     
     private ShoppingListCategory createShoppingListCategory(final String categoryName) {
-        return shoppingListCategoryFactory.create(categoryName);
+        final ShoppingListCategory shoppingListCategory = shoppingListCategoryFactory.create(categoryName);
+        shoppingListCategory.init();
+        shoppingListCategories.put(categoryName, shoppingListCategory);
+        return shoppingListCategory;
     }
     
     private ItemCategory getCategoryFromPersistence(final Item item) {
@@ -100,8 +109,6 @@ public class ShoppingListPresenter extends Presenter<ShoppingList> implements Se
         ShoppingListCategory shoppingListCategory = shoppingListCategories.get(categoryName);
         if (shoppingListCategory == null) {
             shoppingListCategory = createShoppingListCategory(categoryName);
-            shoppingListCategory.init();
-            shoppingListCategories.put(categoryName, shoppingListCategory);
         }
         return shoppingListCategory;
     }
